72d9b0f9d5376d81c46e79968475e50864fb10cc
yoo9857 <hanbin9857@gmail.com>
2025-07-21 08:39:27 +0900

    0.94

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_JWT_SECRET: process.env.NEXT_PUBLIC_JWT_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
   },
   /* config options here */
 };
