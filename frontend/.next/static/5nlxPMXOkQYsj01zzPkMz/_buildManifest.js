cbac72fc7000eab63db1dd255e76e8d58ba46451
yoo9857 <hanbin9857@gmail.com>
2025-07-21 09:09:09 +0900

    0.99

---
+++ frontend/.next/static/5nlxPMXOkQYsj01zzPkMz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST=function(e,r){return{__rewrites:{afterFiles:[{has:e,source:r,destination:r},{has:e,source:"/api/:path*",destination:e}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7ce03bcf1df914ce.js"],sortedPages:["/_app","/_error"]}}(void 0,"/api/auth/:path*"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
